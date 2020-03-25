d9e5e6357d473936a7ed8d94c90bf3ac92d8b32d
ErikaLep <erika.lepisto@metropolia.fi>
2020-03-25 13:58:06 +0200

    Tallenna-nappi asettaa kuluneen ajan konsoliin

--- Kello/kello.js
+++ Kello/kello.js
@@ -81,6 +81,14 @@ hours = (hours < 10) ? "0" + hours : hours;
 }
 function saveExercise(){
 
-  console.log(savedTime);
+  var hours = Math.floor((savedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((savedTime % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((savedTime % (1000 * 60)) / 1000);
+  var milliseconds = Math.floor((savedTime % (1000 * 60)) / 100);
+hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
+  console.log(hours + ':' + minutes + ':' + seconds + ':' + milliseconds);
   
 }

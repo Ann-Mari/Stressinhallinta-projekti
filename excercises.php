5b25fc26d65cf15a33a30f10ba5c45197968ad85
ErikaLep <erika.lepisto@metropolia.fi>
2020-03-26 11:07:31 +0200

    en tiedä mitä teen, jotain ajaxia rakennan

--- excercises.php
+++ excercises.php
@@ -7,27 +7,24 @@ include("config/https.php");
 <html>
 
 <body>
-<?php
-//haettu pohja minun tekemästä lab4:sta
-//Kaikki treeenit SELECT * FROM harjoitukset
-//Jooga SELECT * FROM harjoitukset WHERE harjoitusKoodi = 1
-//Meditaatio SELECT * FROM harjoitukset WHERE harjoitusKoodi = 2
-//halutaan muuttaa käymään läpi harjoituslista ja hakee sieltä kaikki jooga treenit, 
-//laitetaan jokainen palautus omaan diviin, johon tulee youtubevideo
-$tuoteID=2;
-$kyselyTreeni = $DBH->prepare("SELECT vk_tuotteet.nimi, vk_tuotteet.tuotekoodi FROM vk_tuotteet WHERE vk_tuotteet.tID = :haluttuID");
-
-$kyselyTreeni->bindParam(':haluttuID', $tuoteID);
-$kyselyTreeni->execute();
-$kyselyTreeni->setFetchMode(PDO::FETCH_OBJ);
-$ekaTulosOlio = $kyselyTreeni->fetch();   
-
-//Tuloksena on nyt vain vain yksi rivi (vain yksi tuote  tID:llä 2)
-
-echo ("<br>Treeni. Haluttu tID = $tuoteID : " . $ekaTulosOlio->nimi .", tuotekoodi  " . $ekaTulosOlio->tuotekoodi );
-
-?>
+<button type="button" id="btnKaikki">Näytä Kaikki</button>
+<button type="button" id="btnJooga">Näytä jooga-harjoitukset</button>
+<button type="button" id="btnMeditaatio">Näytä meditaatio-harjoitukset</button>
 
 
+<script>
+     const element2 = document.querySelector('#btnKaikki');
+  element2.addEventListener('click',  (evt) => {
+   fetch('kaikkiHarjoitukset.php')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => { 
+          console.log(data);
+          document.getElementById("ajaxText").innerHTML = data;
+          document.getElementById("legText").innerHTML = "2. Sample Pulse: pulseSample.php";
+    });
+  });
+</script>
 </body>
 </html>

f28e4ce867ff298b1090046017e38246cc5b9113
ErikaLep <erika.lepisto@metropolia.fi>
2020-03-26 09:37:48 +0200

    haettu pohja tietokantahakuun

--- excercises.php
+++ excercises.php
@@ -8,6 +8,20 @@ include("config/https.php");
 
 <body>
 <?php
+//haettu pohja minun tekemästä lab4:sta
+//halutaan muuttaa käymään läpi harjoituslista ja hakee sieltä kaikki jooga treenit, 
+//laitetaan jokainen palautus omaan diviin, johon tulee youtubevideo
+$tuoteID=2;
+$kysely3 = $DBH->prepare("SELECT vk_tuotteet.nimi, vk_tuotteet.tuotekoodi FROM vk_tuotteet WHERE vk_tuotteet.tID = :haluttuID");
+
+$kysely3->bindParam(':haluttuID', $tuoteID);
+$kysely3->execute();
+$kysely3->setFetchMode(PDO::FETCH_OBJ);
+$ekaTulosOlio = $kysely3->fetch();   
+
+//Tuloksena on nyt vain vain yksi rivi (vain yksi tuote  tID:llä 2)
+
+echo ("<br>3. Haluttu tID = $tuoteID : " . $ekaTulosOlio->nimi .", tuotekoodi  " . $ekaTulosOlio->tuotekoodi );
 
 ?>
 

8065a4e64dcf4461a3e12012afb02a6ab6372f10
ErikaLep <erika.lepisto@metropolia.fi>
2020-03-31 11:56:17 +0300

    nytten siellä on ainakin jotain

--- harjoitukset/functions.php
+++ harjoitukset/functions.php
@@ -1,18 +1,35 @@
 <?
 
-function trainingText($trainingNumber) {
+function trainingText() {
     $treeniOhjeet = array(
     "iwgWzOoJ1EI", 
     "Qe-25gXKkdo", 
     "mTlqAv2ApkE", 
     "41Frx0YOGDM", 
 );
-    while($i=0; $i<=$treeniOhjeet; $i++){
+    while($i=0, $i<=$treeniOhjeet, ){
 
- echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/"$treeniOhjeet[$i]"' frameborder='0' allowfullscreen></iframe>";
+ echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/"$treeniOhjeet[$i]"' frameborder='0' allowfullscreen></iframe>";$i++
     }
-      
-    return $treeniOhjeet[$trainingNumber-1];
+
+    <script>
+console.log("scripti alkaa");
+ $treeniOhjeet = array(
+    "iwgWzOoJ1EI", 
+    "Qe-25gXKkdo", 
+    "mTlqAv2ApkE", 
+    "41Frx0YOGDM");
+
+function treenit(trainingText){
+    console.log("klikkaus aloitti loopin");
+    var $i=0; 
+    while($i<=$treeniOhjeet){
+        
+ echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/"+$treeniOhjeet[$i]+"' frameborder='0' allowfullscreen></iframe>";
+    $i++;
+    }
+}      
+</script>
  
 
    

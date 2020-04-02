649d708fa0bb5225a3817e8bad746b5eac79584c
Ann-Mari <55827351+Ann-Mari@users.noreply.github.com>
2020-04-02 10:13:05 +0300

    Personalinformation lomake

---
+++ forms/fpersonalinformation.php
@@ -0,0 +1,30 @@
+<fieldset><legend>Henkilötietojen muokkaus</legend>
+<form method="post">
+  <p>
+  Ikä
+  <br />  <input type="text" name="givenAge" placeholder="ikä numeroina" maxlength="40" required/>
+  </p><p>
+  Sukupuoli
+  <br />  <input type="text" name="givenGender" placeholder="mies, nainen tai muu " maxlength="40" required/>
+  </p><p>
+  Pituus
+  <br />  <input type="text" name="givenHeight" placeholder="  " maxlength="40"/>
+  </p><p>
+  Paino
+  <br />  <input type="text" name="givenWeight" placeholder="  " maxlength="40"/>
+  </p><p>
+  Kuinka stressaantunut olet? 
+  <br />  <input type="text" name="givenStress" placeholder="arvioi numeroina 1-10 " maxlength="40"/>
+  </p><p>
+  Taustaa meditaatiosta
+  <br />  <input type="text" name="givenMeditation" placeholder="  " maxlength="40"/>
+  </p><p>
+  Yleiskunto
+  <br />  <input type="text" name="givenCondition" placeholder="esim. hyvä, perus ja huono  " maxlength="40"/>
+  </p><p>
+  <br />  <input type="submit" name="submitUser" value="Hyväksy"/>
+          <input type="reset"  value="Tyhjennä"/>
+          <input type="submit" name="submitBack" value="Palaa myöhemmin"/>
+  </p>
+</form>
+</fieldset>

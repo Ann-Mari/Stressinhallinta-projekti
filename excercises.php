944cfdc8c7f029c584445b0d9b5af659b692029c
Joonakak <joonakak@metropolia.fi>
2020-04-02 11:36:06 +0300

    Css muutoksii nopeesti

--- excercises.php
+++ excercises.php
@@ -5,38 +5,32 @@ include('./includes/inavindex.php');
 
 ?>
 
-<body>
-  
+
 <br>
 <br>
 <br>
-<button type="button" id="btnKaikki" onclick="nextImage()">Näytä Kaikki</button>
-<button type="button" id="btnJooga">Näytä jooga-harjoitukset</button>
-<button type="button" id="btnMeditaatio">Näytä meditaatio-harjoitukset</button>
+
 <?php
 //include_once("functions.php");
 ?>
-<!DOCTYPE html>
-<html>
-
-<body>
+<div class="container">
 <button type="button" id="btnKaikki">Näytä Kaikki</button>
 <button type="button" id="btnJooga">Näytä jooga-harjoitukset</button>
 <button type="button" id="btnMeditaatio">Näytä meditaatio-harjoitukset</button>
-<div>
+<div class="piilo">
 <iframe width='560' height='315' src='https://www.youtube.com/embed/iwgWzOoJ1EI' frameborder='0' allowfullscreen></iframe>
 </div>
-<div>
+<div class="piilo">
 <iframe width='560' height='315' src='https://www.youtube.com/embed/Qe-25gXKkdo' frameborder='0' allowfullscreen></iframe>
 </div>
-<div>
+<div class="piilo">
     <iframe width='560' height='315' src='https://www.youtube.com/embed/mTlqAv2ApkE' frameborder='0' allowfullscreen></iframe></div>
-<div>
+        <div class="piilo">
     <iframe width='560' height='315' src='https://www.youtube.com/embed/41Frx0YOGDM' frameborder='0' allowfullscreen></iframe></div>
-<div>
+        <div class="piilo">
     <iframe width='560' height='315' src='https://www.youtube.com/embed/wsMvCuXETSU' frameborder='0' allowfullscreen></iframe></div>
-<div>
+        <div class="piilo">
     <iframe width='560' height='315' src='https://www.youtube.com/embed/s-ZA5J67KJM' frameborder='0' allowfullscreen></iframe></div>
-
+</div>
 </body>
 </html>

f912f355eeaec809eb9b910a2be5fd88018c6cce
Joonakak <joonakak@metropolia.fi>
2020-04-07 09:16:43 +0300

    Harjoitukset fiksaus

--- excercises.php
+++ excercises.php
@@ -5,21 +5,10 @@ include('./includes/inavindex.php');
 
 ?>
 
-<body>
-  
-<br>
-<br>
-<br>
-<button type="button" id="btnKaikki" onclick="nextImage()">Näytä Kaikki</button>
-<button type="button" id="btnJooga">Näytä jooga-harjoitukset</button>
-<button type="button" id="btnMeditaatio">Näytä meditaatio-harjoitukset</button>
 <?php
 //include_once("functions.php");
 ?>
-<!DOCTYPE html>
-<html>
 
-<body>
 <button type="button" id="btnKaikki">Näytä Kaikki</button>
 <button type="button" id="btnJooga">Näytä jooga-harjoitukset</button>
 <button type="button" id="btnMeditaatio">Näytä meditaatio-harjoitukset</button>

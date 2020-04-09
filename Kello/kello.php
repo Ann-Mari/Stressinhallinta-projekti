b4170dbaf0d8fd5660b99f37a6a3f5b2125b9e7e
ErikaLep <erika.lepisto@metropolia.fi>
2020-04-09 08:58:05 +0300

    Update kello.php

--- Kello/kello.php
+++ Kello/kello.php
@@ -1,17 +1,11 @@
-<?php
-include("config/config.php");
-include("config/https.php");
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
+<div id="kello">
   <title>Vanilla JS Stopwatch - Javascript Stopwatch</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 <link rel="stylesheet" href="kello.css">
-</head>
-<body>
+
+<div id="kelloToiminta">
 <div id="timerContainer">
 <div class="timer" onclick="startTimer()">Aloita!</div>
 <div class="startTimer reset" onclick="startTimer()" >
@@ -25,5 +19,5 @@ include("config/https.php");
 <div class="saveExercise reset" onclick="saveExercise()">Tallenna</div>
 </div>
 <script type="text/javascript" src="kello.js"></script>
-</body>
-</html>
+</div>
+</div>

2d2a66782954c4efb03f619d8842b50eee8946b7
ErikaLep <erika.lepisto@metropolia.fi>
2020-04-12 19:53:20 +0300

    nyt näkyy video, seuraavaksi tehdään randomisoija joka arvoo minkä näyttää

--- Meditaatio/meditaatioEtusivuHarjoitus.php
+++ Meditaatio/meditaatioEtusivuHarjoitus.php
@@ -1,5 +1,5 @@
 <?php
-include("meditaatioharjoitus.js");
-$s = "tgbNymZ7vqY";
-echo(<iframe width="420" height="345" src= "https://www.youtube.com/embed/$s"</iframe>)
+//include("meditaatioharjoitus.js");
+//$s = "tgbNymZ7vqY";
+echo("<iframe width='560' height='315' src='https://www.youtube.com/embed/Qe-25gXKkdo' frameborder='0' allowfullscreen></iframe>");
 ?>

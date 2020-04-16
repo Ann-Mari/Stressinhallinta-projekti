18b1c470ecb2d59d913e65799d9fe5ef899cfc59
Joonakak <joonakak@metropolia.fi>
2020-04-16 09:29:27 +0300

    Graafin tekoo

--- userDiary.php
+++ userDiary.php
@@ -12,13 +12,34 @@ include('./includes/inavindex.php');
 <p>Tämä on sinun päiväkirjasi</p>
 
 <p>Tähän tulee päiväkirja tilastoja Graafeja, jotka saavat tietonsa tietokannasta. Graafien piirtämiseen käytetään plotlyä.</p>
-<div id="tester" style="width:600px;height:250px;"></div>
+<div id='Kahvin ja alkoholin määrä' style="width:800px;height:300px;"></div>
 <script>
-	TESTER = document.getElementById('tester');
+var trace1 = {
+  x: [1, 2, 3, 4, 5, 6, 7],
+  y: [4, 6, 4, 3, 2, 2, 4],
+  type: 'scatter',
+  name: 'Kahvikuppien määrä'
+  
+};
+
+var trace2 = {
+  x: [1, 2, 3, 4, 5, 6, 7],
+  y: [0, 1, 0, 2, 4, 12, 0],
+  type: 'scatter',
+  name: 'Alkoholiannosten määrä'
+  
+};
+
+var data = [trace1, trace2];
+
+Plotly.newPlot('Kahvin ja alkoholin määrä', data);
+/*
+	TESTER = document.getElementById('Kahvi ');
 	Plotly.newPlot( TESTER, [{
 	x: [1, 2, 3, 4, 5],
 	y: [1, 2, 4, 8, 16] }], {
 	margin: { t: 0 } } );
+	*/
 </script>
 
 </main>

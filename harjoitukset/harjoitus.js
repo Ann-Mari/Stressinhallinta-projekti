67bae28ad7b4e3d02e0f53d0d172d281f2ba55af
ErikaLep <erika.lepisto@metropolia.fi>
2020-04-22 12:03:48 +0300

    jos keksitte mikä on vialla

---
+++ harjoitukset/harjoitus.js
@@ -0,0 +1,51 @@
+//Otettu peruskoodi Karin lab7:sata
+//ensiksi haetaan kaikki harjoitukset
+const element1 = document.querySelector('#btnKaikki');
+element1.addEventListener('click',  (evt) => {
+ fetch('ajax/kaikki.php')
+  .then((response) => {
+    console.log("painettu Kaikki");
+    return response.json();
+  })
+  .then((data) => { 
+        console.log(data);
+        document.getElementById("ajaxText").innerHTML = data;
+  });
+});
+
+//nappia jooga painamalla haetaan kaikki joogaharjoitukset
+const element2 = document.querySelector('#btnJooga');
+element2.addEventListener('click',  (evt) => {
+ fetch('ajax/jooga.php')
+  .then((response) => {
+    console.log("painettu jooga");
+    return response.json();
+  })
+  .then((data) => { 
+        console.log(data);
+        let obj = JSON.parse(data);
+        let textKolmas="";
+        for(let i=0; i<3; i++){
+            //Käytetään Polarin alkuperäistä formaattia, 
+            //jossa kaikki on yhden elementin taulukoita
+            textKolmas += "<div class='piilo'><iframe width='560' height='315' src='https://www.youtube.com/embed/" + obj[i].ID + "' frameborder='0' allowfullscreen></iframe></div><div class='piilo'>";
+        }
+        document.getElementById("ajaxText").innerHTML = textKolmas;
+        document.getElementById("legText").innerHTML = "Jooga harjoitukset";
+  });
+});
+
+//nappia painamalla haetaan kaikki meditaatioharjoitukset
+const element3 = document.querySelector('#btnMeditaatio');
+element3.addEventListener('click',  (evt) => {
+ fetch('ajax/meditaatio.php')
+  .then((response) => {
+    console.log("painettu Meditaatio");
+    return response.json();
+  })
+  .then((data) => { 
+        console.log(data);
+        document.getElementById("ajaxText").innerHTML = data;
+        document.getElementById("legText").innerHTML = "Meditaatio harjoitukset";
+  });
+});

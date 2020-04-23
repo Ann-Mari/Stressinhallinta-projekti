e51c66849b2925603be95b5a97d171e7c833f320
Joonakak <joonakak@metropolia.fi>
2020-04-23 09:10:47 +0300

    Kommentteina lisätty tietokannasta haku

--- rest/haeData.php
+++ rest/haeData.php
@@ -6,6 +6,20 @@ session_start();
   
  //Tietojen haku tietokannasta
 
+ /*$data1['email'] = $_SESSION['semail'];
+ $sql1 = "SELECT personalID FROM userRegister where userEmail  =  :email";
+ $kysely1=$DBH->prepare($sql1);
+ $kysely1->execute($data1);
+ $tulos1=$kysely1->fetch();
+ $currentpersonalID=$tulos1[0];
+
+
+$data2['personalID'] = $currentpersonalID;
+$sql = "SELECT paivanFiilis, kofeiini, alkoholi, uni, unenLaatu FROM Paivan_Fiilis WHERE personalID = :personalID";
+$kysely = $DBH->prepare($sql);
+$kysely->execute($data2);
+*/
+
   $paivat = array("Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai");
   $paivanF = array(5,8,9,6,5,4,4);
 //if($_GET['paivat']==7)

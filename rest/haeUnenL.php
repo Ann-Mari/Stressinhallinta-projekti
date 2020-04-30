3287e94491a958f50dc65bad68a7e788b6762ef2
Joonakak <joonakak@metropolia.fi>
2020-04-30 09:52:10 +0300

    SE TOIMII

---
+++ rest/haeUnenL.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+include("../config/config.php");
+
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  
+ //Tietojen haku tietokannasta
+
+
+ $data1['email'] = $_SESSION['semail'];
+ $sql1 = "SELECT personalID FROM userRegister where userEmail  =  :email";
+ $kysely1=$DBH->prepare($sql1);
+ $kysely1->execute($data1);
+ $tulos1=$kysely1->fetch();
+ $currentpersonalID=$tulos1[0];
+
+
+$data2['personalID'] = $currentpersonalID;
+$sql = "SELECT paivanFiilis, kofeiini, alkoholi, uni, unenLaatu FROM Paivan_Fiilis WHERE personalID = :personalID";
+$kysely = $DBH->prepare($sql);
+$kysely->execute($data2);
+
+$paivat = array("Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai");
+$paivanUl = array();
+
+
+while ($row=$kysely->fetch()){
+    $paivanUl[] = $row["unenLaatu"];
+
+}
+
+$PUlaatu = array( 
+    "x" => $paivat,
+    "y" => $paivanUl,
+    "type" => "scatter",
+    "name" => 'Unen laatu'  
+);
+echo(json_encode($PUlaatu));
+
+?>

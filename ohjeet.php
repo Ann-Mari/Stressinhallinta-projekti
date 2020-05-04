d0aaf29356ee6f1ad04f5259e6033966891e184f
Joonakak <joonakak@metropolia.fi>
2020-05-04 23:19:40 +0300

    Ohjeet

--- ohjeet.php
+++ ohjeet.php
@@ -2,7 +2,79 @@
 include("includes/iheader.php");
 include("includes/inavindex.php");
 ?>
+<div class="container">
+<h5>Ohjeet</h5>
+<br>
+<p>Tämän web-sovelluksen tarkoituksena on toimia apuvälineenä hyvinvoinnissa</p>
+<br>
+<br>
+<h5>Etusivu:</h5>
+<br>
+<p>Etusivulta löydät Aloita mittaus, Päivän harjoitusohje ja Päivän fiilis palkit, joista voit valitsemalla Aloita mittauksen mitata harjoituksen keston aikaa ja kirjata kuinka kauan olet harjoitellut.
+ Päivän harjoitusohjeesta löydät sinulle esitetyn harjoituksen. Päivän fiilikseen voit täyttää päivittäisen fiiliksen, 
+ alkoholin ja kahvin kulutuksen sekä unen määrän ja laadun.
+ Päivän fiilikseen ja aloita mittaukseen täytetyt tiedot siirtyvät Päiväkirjaan, josta voit tutkia omia tietoja.</p>
+<br>
+<br>
+<h5>Valikko:</h5>
+<br>
+<p>Valikosta löydät kaikki sovellukseen kuuluvat sivut profiilin, 
+päiväkirjan, harjoitusohjeen, yleiset tiedot ja kirjaudu ulos painikkeen.</p>
+<br><br>
 
+<h5>Profiilisivu:</h5>
+<br>
+<p>Profiilista löydät henkilökohtaiset tiedot taulukkona ja pystyt vaihtamaan kuntotason ja painon. Profiilista pystyt myös vaihtamaan salasanan.</p>
+<br>
+<br>
+<h5>Päiväkirja</h5>
+<br>
+<p>Päiväkirjasta löydät aikaisemmin tallentamasi tiedot toteutuneista harjoituksista sekä täyttämästäsi Päivän fiilis lomakkeesta.
+ Näitä hyödyntämällä voit nähdä muutoksia omassa jaksamisessasi fiiliksen tai ihan vain unen laadun perusteella. </p>
+ <p>Päiväkirjasta löydät aikaisemmin tallentamasi tiedot toteutuneista harjoituksista sekä täyttämästäsi Päivän fiilis lomakkeesta.
+  Näitä hyödyntämällä voit nähdä muutoksia omassa jaksamisessasi fiiliksen tai ihan vain unen laadun perusteella. </p>
+  <p>Unen laatuun vaikuttaa monet asiat mutta muutamia elämäntapoja muuttaminen voi parantaa sitä tehokkaasti.
+   Tärkeimpinä näissä on kofeiniin määrä kehossa, alkoholi ja liikunta.</p>
+   <p>Liikunta väsyttää kehoa fyysisesti ja se tutkitusti nopeuttaa nukahtamista ja pidentää syvien unenjaksojen kestoa.
+    Lisätietoa voit lukea täältä <a href="https://www.kaypahoito.fi/nix02248">Käypahoito</a> </p>
+    <p>Alkoholi lyhentää nukahtamisvaihetta mutta heikentää unen itsensä laatua. </p>
+    <p>Kofeiinia käytetään hereillä pysymiseen sekä virkeyteen, mutta liian myöhään nautittuna se voi viivästyttää nukahtamissyklin alkamista. </p>
+    <br>
+    <a href="https://www.mielenterveystalo.fi/aikuiset/itsehoito-ja-oppaat/itsehoito/unettomuuden_omahoito/Pages/unettomuuden_omahoito_1_mita_unettomuus_on.aspx">Unettomuuden omahoitoon apuvälineitä</a>
+
+<br><br>
+<h5>Harjoitussivu:</h5>
+<br>
+<p>Harjoitussivulta löydät meditaatio ja jooga harjoituksia. Voit valita niistä mieleisesi ja toteuttaa harjoituksen omalla ajallasi. </p>
+
+<h4>Tietoa stressistä:</h4>
+
+<br><br>
+
+<h5>Uni</h5>
+<br>
+<p>Stressi voi aiheuttaa unettomuutta ja pitkään jatkunut unettomuus voi myös aiheuttaa stressiä.
+ Stressi vaikuttaa haitallisesti unen laatuun lisäämällä stressihormonien kasvua ja sitä kautta nostaa vireystasoa.
+ Unen laatua voi parantaa vähentämällä kahvin ja alkoholin kulutusta sekä lisäämällä liikuntaa elämän tapoihinsa.</p>
+ <br>
+ <br>
+ <h5>Liikunta</h5>
+ <br>
+ <p>Liikunta auttaa stressinhallintaan.
+  Liikunnan avulla pystyy helpottamaan stressiä varsinkin meditaatio ja jooga ovat hyviä keinoja hallita stressitasoja.</p>
+  <br>
+  <br>
+  <h5>Meditaatio:</h5>
+  <br>
+  <p>Meditaatio on hyvä keino hallita stressiä. Se auttaa rauhoittamaan ja rentouttamaan sekä parantaa ajatuksenjuoksua.
+   Oman mielen hallinnalla pystyy ylläpitämään omaa hyvinvointiaan ja vähentämään huomattavasti stressiä.</p>
+   <br>
+   <br>
+   <br>
+
+
+
+</div>
 <?php
 include("includes/ifooter.php");
 ?>
